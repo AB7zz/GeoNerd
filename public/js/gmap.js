3a9916df8d894b89ab7523d2efa1eadb8b23e56b
ab7z <abhiksi613@gmail.com>
2021-12-15 11:07:12 +0400

    last

--- public/js/gmap.js
+++ public/js/gmap.js
@@ -1,8 +1,5 @@
 const confirmPin = document.getElementById('confirmPin');
-// let lol = 0;
 function initMap(){
-    // lol++;
-    // console.log('initMap() is called', lol, 'times');
     confirmPin.disabled = true;
     let myLatLng;
     let zoomVal;
@@ -88,22 +85,6 @@ function calcDistance(lat1,lat2,lon1,lon2)
 }
 confirmPin.addEventListener('click', () => {
     const origin = parseFloat(document.getElementById("latitude").value) + ',' + parseFloat(document.getElementById("longitude").value);
-    // console.log('Origin:', origin);
-
-    // const revGeo = 'https://open.mapquestapi.com/geocoding/v1/reverse?key=VhCAzoCdq1iw3EUcNRezRhbXmIdsfxq9&location='+document.getElementById('latitude').value+','+document.getElementById('longitude').value+'&includeRoadMetadata=true&includeNearestIntersection=true';
-    // fetch(revGeo)
-    //     .then(function(response){
-    //         return response.json();
-    //     })
-    //     .then(async function(data){
-    //         const origin = data.results[0].locations[0].adminArea1 + ' ' + data.results[0].locations[0].adminArea3 + ' ' + data.results[0].locations[0].adminArea4 + ' ' + data.results[0].locations[0].adminArea5 + ' ' + data.results[0].locations[0].adminArea6 + ' ' + data.results[0].locations[0].street;
-    //         document.getElementById('address').value = origin;
-            
-    //         });
-    //     }
-    // );
-
-    // const bounds = new google.maps.LatLngBounds();
     const markersArray = [];
     let score = 0;
     map = new google.maps.Map(document.getElementById("map"), {
@@ -134,55 +115,11 @@ confirmPin.addEventListener('click', () => {
         ],
         map: map,
     });
-    // const geocoder = new google.maps.Geocoder();
-    // const service = new google.maps.DistanceMatrixService();
-    
-    
-    // const request = {
-    //     origins: [origin],
-    //     destinations: [destination],
-    //     travelMode: google.maps.TravelMode.DRIVING,
-    //     unitSystem: google.maps.UnitSystem.METRIC,
-    //     avoidHighways: false,
-    //     avoidTolls: false,
-    // };
-    
-    
-    
-    // service.getDistanceMatrix(request).then(async (response) => {
-        // console.log(response);
-        // let string = response.rows[0].elements[0].status;
-        // let status = string.replace(/["]+/g,     '');
-        // if(status!="ZERO_RESULTS"){
-            // document.getElementById("distance").innerText = response.rows[0].elements[0].distance.text.replace(/["]+/g, '');
-            // console.log(response);
-        // }else{
             calcDistance(document.getElementById('latitude').value,document.getElementById('destLat').value,document.getElementById('longitude').value,document.getElementById('destLong').value);
             document.getElementById("distance").innerText = Math.round(distance).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-            // console.log('ERROR: Google API did not work', distance);
-            // console.log(playerId);
             socket.emit('score-inc', ({playerId, roomId, distance}));
-        // }
-
-        
-        // const originList = response.originAddresses;
-        // const destinationList = response.destinationAddresses;
 
         deleteMarkers(markersArray);
-
-        // const showGeocodedAddressOnMap = (asDestination) => {
-        //     const handler = ({ results }) => {
-                   // map.fitBounds(bounds.extend(results[0].geometry.location));
-        //         markersArray.push(
-        //             new google.maps.Marker({
-        //                 map,
-        //                 position: results[0].geometry.location,
-        //                 icon: asDestination ? "images/greenmark.png" : "images/redmark.png",
-        //             })
-        //         );
-        //     };
-        //     return handler;
-        // };
 
         
         const handler = (asDestination) => {
@@ -194,27 +131,6 @@ confirmPin.addEventListener('click', () => {
                 })
             );
         };
-
-        // for (let i = 0; i < originList.length; i++) {
-        //     const results = response.rows[i].elements;
-        //     geocoder
-        //     .geocode({ address: originList[i] })
-        //     .then(showGeocodedAddressOnMap(false));
-
-        //     for (let j = 0; j < results.length; j++) {
-        //         geocoder
-        //         .geocode({ address: destinationList[j] })
-        //         .then(showGeocodedAddressOnMap(true));
-        //     }
-        // }
-
-        // for (let i = 0; i < 1; i++) {
-        //     handler(false);
-        //     for (let j = 0; j < 1; j++) {
-        //         handler(true);
-        //     }
-        //     return handler;
-        // }
         
         handler(false); 
         handler(true);
@@ -224,6 +140,5 @@ confirmPin.addEventListener('click', () => {
                 markersArray[i].setMap(null);
             }
             markersArray = [];
-        }  
-    // });                                         
+        }                                         
 })
